4ede16cd7dd06f3a7ffa929b185fd7adcb51e969
nishantyadav24 <ynishant567@gmail.com>
2024-12-09 22:57:27 +0530

    wildcard-error-hanling-lstapp.use

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use('/user',
-    (req,res,next)=>{
-    //request handler 
-    console.log("handliong first route")
-
-    next();
-    res.send("route handler 1  ")
-})
+// app.use('/admin',adminAuth)
+// app.use('/admin',userAuth )
 //     (req,res)=>{
 //     //request handler 
 //     console.log("handling second route")
@@ -35,12 +30,41 @@ app.use('/user',
 // })
 //order of wrinting the routes matter a lot
 
-app.use('/user',
+app.get('/user/admin/getAllData',
 (req,res)=>{
     //request handler 
-    console.log("handling third route")
+   
+
+    res.send("all data sent ")})
+app.get('/user/admin/deleteUser',
+(req,res)=>{
+    //check first if the request is sautodrized
+    console.log("handling delete route")
 
     res.send("route handler 3 ")})
+app.get('/user/admin/SaveAllData',
+(req,res)=>{
+    //request handler 
+    console.log("handling daata save route")
+
+    res.send("route handler 3 ")})
+//wildcard error handling
+    app.use('/',(error,req,res,next)=>{
+        if(error){
+            res.status(500).send("somethingn went wrong ")
+        }
+    })
+// app.get("/getUserData",(req,res)=>{
+//     try{
+//         //logic of db call 
+
+//          throw new Error("sdasda")
+//     } 
+//     catch(error){
+//         res.status(500).send("contact unable to restablish  ")
+//     }
+// })
+
 app.listen(3000,()=>{
     console.log("server is sucessfully listening on this port  ")
 })
---
+++ src/sastaapp.js
@@ -0,0 +1,31 @@
+// const express = require('express')
+// const app = express()
+
+
+
+// //you can create middle ware  using the app.use function 
+// // GET  /users  => goes through a middleware chain  =>  request handle will handle after parsing all the middlewares
+// app.use('/', (req,res,next)={
+// next();
+
+
+
+// });
+
+
+// //middlware are fucntion that can be used to authenticate or fvalidate or various fucntion
+// //you can pass your api throgh other middlwares using next each middleware has a particular function 
+
+// app.get('/users', (req,res,next)=>{
+//     cosnt 
+
+//     res.send("response sent by request hadnler 1`")
+
+
+
+// })
+
+
+// app.listen(3000,()=>{
+
+// })
