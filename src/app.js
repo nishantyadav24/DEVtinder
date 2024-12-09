cd347b79cb07bd6ca53fb019104a5575da239f69
nishantyadav24 <ynishant567@gmail.com>
2024-12-09 17:43:04 +0530

    learning routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.use('/hit',(req,res)=>{
+app.use('/user',
+    (req,res,next)=>{
+    //request handler 
+    console.log("handliong first route")
 
-    res.send("Helloo from the server ")
+    next();
+    res.send("route handler 1  ")
 })
+//     (req,res)=>{
+//     //request handler 
+//     console.log("handling second route")
 
+//     res.send("route handler 2  ")
+// })
+    // next();
+// },
+//     (req,res)=>{
+//     //request handler 
+
+//     // res.send("route handler 1  ")
+// },
+//     (req,res)=>{
+//     //request handler 
+
+//     // res.send("route handler 1  ")
+// },
+//     (req,res)=>{
+//     //request handler 
+
+//     // res.send("route handler 1  ")
+// })
+//order of wrinting the routes matter a lot
+
+app.use('/user',
+(req,res)=>{
+    //request handler 
+    console.log("handling third route")
+
+    res.send("route handler 3 ")})
 app.listen(3000,()=>{
     console.log("server is sucessfully listening on this port  ")
 })
